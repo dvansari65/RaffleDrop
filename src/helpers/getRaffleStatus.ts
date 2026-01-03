8fc5892134edb51ad2235a1ea0fd38f1329247a1
Danish Ansari <dvansari360@gmail.com>
2026-01-03 19:13:37 +0530

    feat:created helper to get proper status

---
+++ src/helpers/getRaffleStatus.ts
@@ -0,0 +1,11 @@
+import { RaffleStatus } from "@/types/raffle";
+
+
+export const getRaffleStatus = (status: any): RaffleStatus => {
+    if (status.active) return "active";
+    if (status.drawing) return "drawing";
+    if (status.completed) return "completed";
+    if (status.cancelled) return "cancelled";
+    if (status.refunded) return "refunded";
+    throw new Error("Invalid raffle status");
+  };
