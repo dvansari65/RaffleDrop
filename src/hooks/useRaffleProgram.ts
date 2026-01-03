bb8246714ea4e325a3cda33a8821509e90ac7f62
Danish Ansari <dvansari360@gmail.com>
2026-01-03 08:43:48 +0530

    fix:added strict typing

--- src/hooks/useRaffleProgram.ts
+++ src/hooks/useRaffleProgram.ts
@@ -1,14 +1,31 @@
 import { AnchorProvider, Program } from "@coral-xyz/anchor"
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
-import idl from "../idl/basic.json"
+import idl from "../idl/Raffle.json"
+import { useMemo } from "react";
+import { Raffle } from "@/types/RaffleTypes";
 
 export const useRaffleProgram = ()=>{
-    const {connection} = useConnection()
-    const {wallet} = useWallet()
-    const provider = new AnchorProvider(connection,wallet as any,{
-        commitment:"confirmed"
-    })
-    const program = new Program(idl,provider)
+    const { connection } = useConnection();
+    const wallet = useWallet();
+  
+    const provider = useMemo(() => {
+      if (!wallet.wallet) return null;
+  
+      return new AnchorProvider(
+        connection,
+        wallet as any,
+        { commitment: "confirmed" }
+      );
+    }, [connection, wallet]);
+  
+    const program = useMemo(() => {
+      if (!provider) return null;
+  
+      return new Program<Raffle>(
+        idl as Raffle,
+        provider
+      );
+    }, [provider]);
     return {
         program,
         provider
