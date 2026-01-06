55c8ef99a76eddd0cfdfebba640910afd2e16d0d
Danish Ansari <dvansari360@gmail.com>
2026-01-06 16:19:22 +0530

    update: added counter account

--- src/api/createRaffle.ts
+++ src/api/createRaffle.ts
@@ -1,5 +1,8 @@
+
+import { getCounterPubkey } from "@/helpers/getCounterPubkey"
 import { getPaymentMint } from "@/helpers/getPaymentMint"
 import { uploadToIPFS } from "@/helpers/uploadToIPFS"
+import { useGetCounter } from "@/hooks/getCounter"
 import { useCreateAssociatedToken } from "@/hooks/useCreateATA"
 import { useRaffleProgram } from "@/hooks/useRaffleProgram"
 import { CreateRaffleInputs } from "@/types/raffle"
@@ -8,6 +11,7 @@ import { useWallet } from "@solana/wallet-adapter-react"
 import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js"
 import { useMutation } from "@tanstack/react-query"
 import { BN } from "bn.js"
+import { toast } from "sonner"
 
 
 export const CreateRaffle = ()=>{
@@ -15,7 +19,8 @@ export const CreateRaffle = ()=>{
     const {program} = useRaffleProgram()
     const {publicKey} = useWallet()
     const paymentMint = getPaymentMint()
-  
+    const {getCounterValue} = useGetCounter()
+    const counterKey = getCounterPubkey()
     return useMutation<any,Error,CreateRaffleInputs>({
         mutationKey:["create-raffle"],
         mutationFn:async({
@@ -29,30 +34,31 @@ export const CreateRaffle = ()=>{
             ticketPrice
         }:CreateRaffleInputs)=>{
             try {
+                
                 if(!publicKey){
                     throw new Error("Connect your wallet!")
                 }
+                if(!sellingPrice || !maxTickets || !minTickets || !ticketPrice){
+                    throw new Error("Some fields are missing!")
+                }
                 if(!program?.programId){
                     throw new Error("Program not found!")
+                }
+                const counterValue = await getCounterValue()
+                if(!counterKey){
+                    throw new Error("Counter value not found!")
                 }
                 const sellerTokenAccount = await createTokenAccount()
                 if(!sellerTokenAccount){
                     throw new Error("Seller token account not found!")
                 }
-                const [counterPda] = PublicKey.findProgramAddressSync(
-                    [
-                        Buffer.from("global-counter")
-                    ],
-                    program.programId
-                )
-                if(!counterPda){
-                    throw new Error("counter pda not found!")
-                }
+                
+                
                 const [rafflePda] = PublicKey.findProgramAddressSync(
                     [
                       Buffer.from("raffle"),
                       publicKey.toBuffer(),
-                      counterPda.toBuffer()
+                      counterValue.toArrayLike(Buffer,"le",8)
                     ],
                     program?.programId
                   )
@@ -69,12 +75,18 @@ export const CreateRaffle = ()=>{
                     ],
                     program.programId
                 )
+
                 if(!itemImage){
                     throw new Error("Image not found!")
                 }
-                const cid = await uploadToIPFS(itemImage)
-                  
+                let cid:string;
 
+                try {
+                    cid = await uploadToIPFS(itemImage)
+                } catch (error:any) {
+                    throw new Error(error.message)
+                }
+               
                 const tx = await program.methods
                                 .createRaffle(
                                     itemName,
@@ -82,12 +94,13 @@ export const CreateRaffle = ()=>{
                                     cid,
                                     new BN(sellingPrice),
                                     new BN(ticketPrice),
-                                    minTickets,
+                                    minTickets ,
                                     maxTickets,
                                     new BN(deadline)
                                 )
                                 .accounts({
                                     seller:publicKey,
+                                    counter:counterKey,
                                     paymentMint,
                                     sellerTokenAccount:sellerTokenAccount,
                                     raffleAccount:rafflePda,
@@ -102,6 +115,23 @@ export const CreateRaffle = ()=>{
                 return tx
             } catch (error:any) {
                 console.log("error:",error.message)
+                
+                if(
+                    error.message.includes("counter") &&
+                    error.message.includes("AccountNotInitialized")
+                ){
+                   const tx = await (program.methods as any)
+                            .initialiseCounter()
+                            .accounts({
+                                counter:counterKey,
+                                signer:publicKey,
+                                systemProgram:SystemProgram.programId
+                            })
+                            .rpc()
+                    if(tx){
+                        toast.success("Counter initialised!")
+                    }
+                }
                 throw error
             }
 
