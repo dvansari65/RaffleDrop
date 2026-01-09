d32dac3efdf20f475406a335859a8b3556cbf6f0
Danish Ansari <dvansari360@gmail.com>
2026-01-09 14:22:53 +0530

    feat:created util fn to calculate progress

--- anchor/programs/anchor/src/utils.rs
+++ anchor/programs/anchor/src/utils.rs
@@ -1,21 +1,23 @@
 // utility function to update raffle fields
 
 use crate::{error::RaffleError, types::RaffleAccount};
-
 impl RaffleAccount {
-    pub fn calculate_progress (&mut self,entries:&u64,max_tickets:&u32)->Result< u32 ,RaffleError>{
-        if *max_tickets == 0 {
+    pub fn calculate_progress(entries: u64, max_tickets: u32) -> Result<u32, RaffleError> {
+        if max_tickets == 0 {
             return Ok(0);
         }
-        if self.total_entries >= *entries {
-            return Ok(100 as u32);
+        
+        if entries >= max_tickets as u64 {
+            return Ok(100);
         }
-        let percentage = entries.
-                            checked_mul(100)
-                            .ok_or(RaffleError::Overflow)?;
+        
+        let percentage = entries
+            .checked_mul(100)
+            .ok_or(RaffleError::Overflow)?;
+        
         let progress = percentage
-                            .checked_div(percentage)
-                            .ok_or(RaffleError::UnderFlow)?;
+            .checked_div(max_tickets as u64)
+            .ok_or(RaffleError::UnderFlow)?;
         
         Ok(progress as u32)
     }
